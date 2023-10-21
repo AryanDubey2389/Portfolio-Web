befa3fd562e756c921fba87810d0239d52f7857e
AryanDubey2389 <aryan.dubey@research.iiit.ac.in>
2023-10-22 03:08:42 +0530

    initial commit

---
+++ precache-manifest.3bb8154537ab7fcb10300e44bac12c1d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c136c8ae3ef9bf1942d20fc7b537db79",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6d63ef62c2a90a5fa7ad",
+    "url": "/Portfolio/static/css/main.6750e22f.chunk.css"
+  },
+  {
+    "revision": "f160c1f068c814a16423",
+    "url": "/Portfolio/static/js/2.88a38dbc.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/Portfolio/static/js/2.88a38dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d63ef62c2a90a5fa7ad",
+    "url": "/Portfolio/static/js/main.3abef595.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "41aa1f799d3fa5a0d1e359503428b2db",
+    "url": "/Portfolio/static/media/DP.41aa1f79.jpg"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/Portfolio/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "785958830d9e777e3ba736f357518b05",
+    "url": "/Portfolio/static/media/c-shell.78595883.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/Portfolio/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/Portfolio/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "e72393d3c25723659074b5187d6cc869",
+    "url": "/Portfolio/static/media/centrosis.e72393d3.png"
+  },
+  {
+    "revision": "dc4decce61ce2fc8ff3b0524b4fa7109",
+    "url": "/Portfolio/static/media/cv.dc4decce.pdf"
+  },
+  {
+    "revision": "4db5dc3884b5c87fc62584d220ae770b",
+    "url": "/Portfolio/static/media/i-hub.4db5dc38.png"
+  },
+  {
+    "revision": "9f451564f8a5c3845d66520ab9b7f8ec",
+    "url": "/Portfolio/static/media/job-application-portal.9f451564.png"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/Portfolio/static/media/random.9388d614.png"
+  },
+  {
+    "revision": "000657c69e02afda3705c01d64cd6f64",
+    "url": "/Portfolio/static/media/samsung.000657c6.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/Portfolio/static/media/xyz.9c1952ca.png"
+  }
+]);
